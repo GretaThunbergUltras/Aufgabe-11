85b1ee32c5828dcfae28d1ae44ff420800fc64c3
ReichAndreas <andreas.reich@tecalliance.net>
2019-12-19 17:02:56 +0100

    updated main to create jpg files

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,14 @@
 from Motor import *
+from picamera import PiCamera
 from time import sleep
 
 
 class PiRobot11:
     speed = 2000  # range -4096 => 4096
     motor = Motor()
+    camera = PiCamera()
+    camera.resolution = (1920, 1080)
+    camera.start_preview()
     imageIndex = 0
 
     def driveStraight(self, timeMillis):
@@ -12,8 +16,13 @@ class PiRobot11:
         time.sleep(timeMillis / 1000)
         self.motor.setMotorModel(0, 0, 0, 0)
 
+    def makePhoto(self, number):
+        camera.capture("picture{}.jpg".format(number))
+
 
 robot = PiRobot11()
+robot.makePhoto(0)
 for i in range(0, 3):
     robot.driveStraight(2000)
     time.sleep(0.6)
+    robot.makePhoto(i + 1)
