7ca5d4103e9555362d14ead78905228a6caac7b4
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-01-27 12:19:13 +0100

    initial Main.py

---
+++ Python/Main.py
@@ -0,0 +1,17 @@
+import brickpi3
+from time import sleep
+
+bp = brickpi3.BrickPi3()
+
+def setMotorPower(power):
+    bp.set_motor_power(bp.PORT_B, power)
+
+def captureImage():
+    print("capture")
+
+captureImage()
+for i in range(0, 3):
+    setMotorPower(50)
+    sleep(2.0)
+    setMotorPower(0)
+    captureImage()
