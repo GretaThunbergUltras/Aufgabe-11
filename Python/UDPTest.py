33850ed4d8701273459db6ed896778caec24eefa
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-01-27 12:43:15 +0100

    to_bytes change

--- Python/UDPTest.py
+++ Python/UDPTest.py
@@ -3,4 +3,4 @@ import socket
 receiverIP = "192.168.43.83"
 receiverPort = 32127
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.sendto((49).to_bytes(2, "big"), (receiverIP, receiverPort))
+socket.sendto((49).to_bytes(2, byteorder = "little"), (receiverIP, receiverPort))
