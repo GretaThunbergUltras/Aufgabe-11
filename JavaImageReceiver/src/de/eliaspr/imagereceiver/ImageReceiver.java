75d3054003a2d5cfdb405f84f6110bd0bd8ab584
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-01-27 12:39:32 +0100

    2nd UDP test

--- JavaImageReceiver/src/de/eliaspr/imagereceiver/ImageReceiver.java
+++ JavaImageReceiver/src/de/eliaspr/imagereceiver/ImageReceiver.java
@@ -1,5 +1,7 @@
 package de.eliaspr.imagereceiver;
 
+import de.eliaspr.toolboxx.SerializationUtils;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -9,12 +11,13 @@ public class ImageReceiver {
 
     public static void main(String[] args) throws IOException {
         DatagramSocket socket = new DatagramSocket(32127);
-        byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, 1024);
+        byte[] buffer = new byte[4096];
+        DatagramPacket packet = new DatagramPacket(buffer, 4096);
+        byte[] imageBuffer = new byte[16 * 1024 * 1024];
         while(true) {
             socket.receive(packet);
-            String str = new String(buffer);
-            System.out.println(str + " from " + packet.getAddress().toString() + ":" + packet.getPort());
+            int imageSizeBytes = SerializationUtils.readInt(buffer, 0);
+            System.out.println(imageSizeBytes);
         }
     }
 
