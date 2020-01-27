bd58a4965d63f7f1df0444a19d0612e19e46b652
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-01-27 13:11:49 +0100

    first image sending test

--- JavaImageReceiver/src/de/eliaspr/imagereceiver/ImageReceiver.java
+++ JavaImageReceiver/src/de/eliaspr/imagereceiver/ImageReceiver.java
@@ -1,5 +1,9 @@
 package de.eliaspr.imagereceiver;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,11 +14,21 @@ public class ImageReceiver {
         DatagramSocket socket = new DatagramSocket(32127);
         byte[] buffer = new byte[4096];
         DatagramPacket packet = new DatagramPacket(buffer, 4096);
-        byte[] imageBuffer = new byte[16 * 1024 * 1024];
-        while(true) {
+        while (true) {
             socket.receive(packet);
             int imageSizeBytes = SerializationUtils.readInt(buffer, 0);
-            System.out.println(imageSizeBytes);
+            int imagePacketCount = SerializationUtils.readInt(buffer, 4);
+            byte[] imageBuffer = new byte[imageSizeBytes];
+            for (int i = 0; i < imagePacketCount; i++) {
+                socket.receive(packet);
+                int packetIndex = SerializationUtils.readInt(buffer, 0);
+                int packetSize = SerializationUtils.readInt(buffer, 4);
+                int packetOffset = packetIndex * 4000;
+                System.arraycopy(buffer, 8, imageBuffer, packetOffset, packetSize);
+            }
+
+            BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageBuffer));
+            JOptionPane.showMessageDialog(null, "Test,", "Test", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(image));
         }
     }
 // 48645321
--- Python/Main.py
+++ Python/Main.py
@@ -1,5 +1,17 @@
+from picamera import PiCamera
 import brickpi3
 from time import sleep
+import socket
+import struct
+import math
+
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.start_preview()
+
+receiverIP = "192.168.43.83"
+receiverPort = 32127
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 bp = brickpi3.BrickPi3()
 
@@ -7,7 +19,19 @@ def setMotorPower(power):
     bp.set_motor_power(bp.PORT_B, power)
 
 def captureImage():
-    print("capture")
+    camera.capture("temp.jpg")
+    with open("temp.jpg", "rb") as image:
+        f = image.read()
+        b = bytearray(f)
+        byteCount = len(b)
+        packageCount = math.ceil(byteCount / 4000)
+        
+        socket.sendto(struct.pack(">i", byteCount) + struct.pack(">i", packageCount), (receiverIP, receiverPort))
+        for i in range(0, packageCount):
+            packetSize = packageCount % 4000 if i == (packageCount - 1) else 4000
+            packetBytes = b[(4000 * i):(4000*i + packetSize)]
+            socket.sendto(struct.pack(">i", i) + struct.pack(">i", packetSize) + packetBytes, (receiverIP, receiverPort))
+
 
 captureImage()
 for i in range(0, 3):
