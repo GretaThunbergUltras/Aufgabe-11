5b79744072f72b80fe92f1957ab704d48bfe3eca
Gruppe 11 <eli.hoerner.19@lehre.mosbach.dhbw.de>
2020-01-29 16:31:28 +0000

    changed image directory

--- Python/Main.py
+++ Python/Main.py
@@ -10,7 +10,7 @@ camera.start_preview()
 bp = brickpi3.BrickPi3()
 
 def captureImage():
-    camera.capture("image_{}.jpg".format(datetime.now().strftime("%H-%M-%S")))
+    camera.capture("../Images/image_{}.jpg".format(datetime.now().strftime("%H-%M-%S")))
     
 captureImage()
 for i in range(0, 3):
