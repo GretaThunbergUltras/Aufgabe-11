ca2369ac8ffb50535b1d9cefb202b96db09daca5
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-01-29 16:04:25 +0100

    removed UDPTest.py

--- Python/UDPTest.py
+++
@@ -1,7 +0,0 @@
-import socket
-import struct
-
-receiverIP = "192.168.43.83"
-receiverPort = 32127
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.sendto(struct.pack(">i", 2000364432), (receiverIP, receiverPort))
