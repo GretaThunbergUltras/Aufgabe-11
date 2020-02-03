8f35b60e082f4c4251f10edb15d30b00f635bb6d
Elias <elias.hoerner@gmail.com>
2020-02-03 10:33:34 +0100

    MQTT command working (#1)
    * added missing MQTT initializer
    
    * debug prints
    
    * Main2.py changes
    
    * changed channel name
    
    * swapped on_message defs
    
    * changes for test
    
    * removed try/except
    
    * re-added try/except
    
    * re-organized Python files
    
    * WIP changes
    
    * split Main into two files
    
    * added new file forgotten earlier
    
    * copied
    
    * cohanged import
    
    * should work now
    
    * MQTT working!?
    
    * might actually work
    
    * added command as comment

--- Python/Main.py
+++ Python/Main.py
@@ -1,37 +1,65 @@
+import paho.mqtt.client as mqtt
 from botlib.bot import Bot
 from brickpi3 import BrickPi3
 from time import sleep
 from datetime import datetime
 import os
 
-print("Initializing BotLib")
-bot = Bot()
+# mosquitto_pub -h 192.168.178.51 -t img_capture -m execMakePhoto
+MQTT_SERVER = "localhost"
+MQTT_PATH = "img_capture"
 
-print("Calibrating steering motor")
-bot.calibrate()
+def on_connect(client, userdata, flags, rc):
+    print("MQTT > Connected with result code " + str(rc))
+    client.subscribe(MQTT_PATH)
 
-print("Making images directory")
-if not os.path.isdir("../Images/"):
-    os.mkdir("../Images/")
+def on_message(client, userdata, msg):
+    payload = str(msg.payload)
+    print("MQTT > topic\"{}\" payload=\"{}\"".format(msg.topic, payload))
+    if "execMakePhoto" in payload:
+        print("MQTT > Received start command")
+        executeImageCapture()
 
 def captureImage():
     imageDate = datetime.now().strftime("%H-%M-%S")
-    print("Capturing image \"image_{}.jpg\"".format(imageDate))
-    bot._camera._cam.capture("../Images/image_{}.jpg".format(imageDate))
-    
-# Capture first image
-captureImage()
+    fileName = "image_{}.jpg".format(imageDate)
+    print("Capturing image \"{}\"".format(fileName))
+    filePath = "../Images/{}".format(fileName)
+    bot._camera._cam.capture(filePath)
+    # TODO convert image to base64
+    # TODO send image back via MQTT
+    # TODO delete image and temp file
 
-# Capture three images after driving 2 seconds each time
-bp = BrickPi3()
-for i in range(3):
-    print("Iteration {}/3".format(i + 1))
-
-    bp.set_motor_power(bp.PORT_B, 40)
-    sleep(2.0)
-    bp.set_motor_power(bp.PORT_B, 0)
-    sleep(0.5)
+def executeImageCapture():
     captureImage()
 
-print("Stopping Bot")
-bot.stop_all()
+    bp = BrickPi3()
+    for i in range(3):
+        print("Iteration {}/3".format(i + 1))
+
+        bp.set_motor_power(bp.PORT_B, 40)
+        sleep(2.0)
+        bp.set_motor_power(bp.PORT_B, 0)
+        sleep(0.5)
+        captureImage()
+
+print("Initializing BotLib")
+bot = Bot()
+
+try:
+    print("Calibrating motors")
+    bot.calibrate()
+
+    print("Making images directory")
+    if not os.path.isdir("../Images/"):
+        os.mkdir("../Images/")
+
+    print("MQTT > Starting client")
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(MQTT_SERVER, 1883, 60)
+    client.loop_forever()
+except:
+    print("Stopping Bot")
+    bot.stop_all()
