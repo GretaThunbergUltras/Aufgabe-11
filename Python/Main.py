50de63903dfd020aed488380684e7111ac78b349
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-02-03 10:21:22 +0100

    copied

--- Python/Main.py
+++ Python/Main.py
@@ -1,27 +1,30 @@
 import paho.mqtt.client as mqtt
 
 MQTT_SERVER = "localhost"
-MQTT_PATH = "img_capture"
+MQTT_PATH = "test_channel"
 
 def on_connect(client, userdata, flags, rc):
-    print("MQTT > Connected with result code {}".format(rc))
-    client.subsribe(MQTT_PATH)
+    print("Connected with result code"+str(rc))
+    client.subscribe(MQTT_PATH)
+
 
 def on_message(client, userdata, msg):
-    print("received a message")
     payload = str(msg.payload)
-    print("MQTT > topic\"{}\" payload=\"{}\"".format(msg.topic, payload))
+    print(msg.topic+" "+str(payload))
+    print("execMakePhoto" in payload)
     if "execMakePhoto" in payload:
-        print("MQTT > Received start command")
-        import Execute.py
+        print("In Loop")
+        import Main.py
+        print("Success")
+    
 
 try:
-    print("MQTT > Starting client")
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
+
     client.connect(MQTT_SERVER, 1883, 60)
+
     client.loop_forever()
 except KeyboardInterrupt:
-    print("Stopping bot")
-    bot.stop_all()
+    print("STOP!!!")
