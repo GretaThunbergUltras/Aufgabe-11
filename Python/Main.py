86c4002f079f119ec52cce2c04dc90f743ac8216
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-02-03 10:17:44 +0100

    split Main into two files

--- Python/Main.py
+++ Python/Main.py
@@ -1,9 +1,4 @@
 import paho.mqtt.client as mqtt
-from botlib.bot import Bot
-from brickpi3 import BrickPi3
-from time import sleep
-from datetime import datetime
-import os
 
 MQTT_SERVER = "localhost"
 MQTT_PATH = "img_capture"
@@ -18,42 +13,9 @@ def on_message(client, userdata, msg):
     print("MQTT > topic\"{}\" payload=\"{}\"".format(msg.topic, payload))
     if "execMakePhoto" in payload:
         print("MQTT > Received start command")
-        executeMain()
-
-def executeMain():
-    captureImage()
-
-    bp = BrickPi3()
-    for i in range(3):
-        print("Iteration {}/3".format(i + 1))
-
-        bp.set_motor_power(bp.PORT_B, 40)
-        sleep(2.0)
-        bp.set_motor_power(bp.PORT_B, 0)
-        sleep(0.5)
-        captureImage()
-
-def captureImage():
-    imageDate = datetime.now().strftime("%H-%M-%S")
-    fileName = "image_{}.jpg".format(imageDate)
-    print("Capturing image \"{}\"".format(fileName))
-    filePath = "../Images/{}".format(fileName)
-    bot._camera._cam.capture(filePath)
-    # TODO convert image to base64
-    # TODO send image back via MQTT
-    # TODO delete image and temp file
+        import Execute.py
 
 try:
-    print("Initializing BotLib")
-    bot = Bot()
-
-    print("Calibrating motors")
-    bot.calibrate()
-
-    print("Making images directory")
-    if not os.path.isdir("../Images/"):
-        os.mkdir("../Images/")
-
     print("MQTT > Starting client")
     client = mqtt.Client()
     client.on_connect = on_connect
