1a0d4188831289cde8e7e7a03d3ede69375495e5
Elias Hoerner <Elias.Hoerner@wittenstein.de>
2020-02-03 10:26:47 +0100

    MQTT working!?

--- Python/Main.py
+++ Python/Main.py
@@ -1,30 +1,22 @@
 import paho.mqtt.client as mqtt
-from botlib.bot import Bot
 
 MQTT_SERVER = "localhost"
 MQTT_PATH = "img_capture"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print("MQTT > Connected with result code " + str(rc))
     client.subscribe(MQTT_PATH)
-
 
 def on_message(client, userdata, msg):
     payload = str(msg.payload)
-    print(msg.topic + " " + str(payload))
-    print("execMakePhoto" in payload)
+    print("MQTT > topic\"{}\" payload=\"{}\"".format(msg.topic, payload))
     if "execMakePhoto" in payload:
+        print("MQTT > Received start command")
         import Execute.py
 
-try:
-    print("MQTT > Starting client")
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-
-    client.connect(MQTT_SERVER, 1883, 60)
-
-    client.loop_forever()
-except KeyboardInterrupt:
-    print("Stopping bot")
-    Bot().stop_all()
+print("MQTT > Starting client")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER, 1883, 60)
+client.loop_forever()
